c04afb920d7ac05216348afdc8fe1d88af931e89
SONALI <232sonali.sharma@gmail.com>
2019-07-30 14:44:13 +0530

    jenkin

--- Calc_App.java
+++ Calc_App.java
@@ -5,37 +5,9 @@ import java.util.Scanner;
 public class Calc_App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//Scanner sc= new Scanner(System.in);
-		
-		int add=addition(4,5);
-		System.out.print(add);
-		/*int subtract=sub(x,y);
-		int mul=multiply(x,y);
-		int div=divide(x,y);*/
-		
-
-	}
-
-	private static int divide(int x, int y) {
-		// TODO Auto-generated method stub
-		return x+y;
-	}
-
-	private static int multiply(int x, int y) {
-		// TODO Auto-generated method stub
-		return x*y;
-	}
-
-	private static int sub(int x, int y) {
-		// TODO Auto-generated method stub
-		
-		return 0;
-	}
-
-	private static int addition(int x, int y) {
-		// TODO Auto-generated method stub
-		return x+y;
-	}
-
+		for(int i=0;i<5;i++)
+		{
+			System.out.println("hello_world");
+		}
+}
 }
