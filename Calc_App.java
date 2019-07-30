b0c62bb8ac6d6c35c3ce80d4e0fd68c8d7831e64
SONALI <232sonali.sharma@gmail.com>
2019-07-30 11:51:11 +0530

    cal_app

---
+++ Calc_App.java
@@ -0,0 +1,44 @@
+package maven.maven_project;
+
+import java.util.Scanner;
+
+public class Calc_App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Scanner sc= new Scanner(System.in);
+		
+		int add=addition(4,5);
+		System.out.print(add);
+		/*int subtract=sub(x,y);
+		int mul=multiply(x,y);
+		int div=divide(x,y);*/
+		
+
+	}
+
+	private static int divide(int x, int y) {
+		// TODO Auto-generated method stub
+		return x+y;
+	}
+
+	private static int multiply(int x, int y) {
+		// TODO Auto-generated method stub
+		return x*y;
+	}
+
+	private static int sub(int x, int y) {
+		// TODO Auto-generated method stub
+		
+		if (x>y)
+			return x-y;
+		else
+			return y-x;
+	}
+
+	private static int addition(int x, int y) {
+		// TODO Auto-generated method stub
+		return x+y;
+	}
+
+}
